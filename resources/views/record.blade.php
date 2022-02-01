870da84848402af365a07ec9468251dbf73aa8b8
tn-luper <tatsuki.nakategawa@gmail.com>
2022-02-01 16:40:53 +0900

    過去の達成率のページの編集（グラフ内に表示される数値を割合に変更）

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -20,17 +20,17 @@
                 <div class="progress w-75" style="height: 30px;">
                 @foreach ($status1counts as $status1key => $status1value)
                     @if($status1key === $pastkey)
-                        <div class="progress-bar bg-success" role="progressbar" style="width: {{$status1value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status1value}}</font></div>
+                        <div class="progress-bar bg-success" role="progressbar" style="width: {{$status1value / $pastvalue * 100}}%"><font color="black" font size="3">{{round($status1value / $pastvalue * 100)}}%</font></div>
                     @endif
                 @endforeach
                 @foreach ($status2counts as $status2key => $status2value)
                     @if($status2key === $pastkey)
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: {{$status2value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status2value}}</font></div>
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: {{$status2value / $pastvalue * 100}}%"><font color="black" font size="3">{{round($status2value / $pastvalue * 100)}}%</font></div>
                     @endif
                 @endforeach
                 @foreach ($status3counts as $status3key => $status3value)
                     @if($status3key === $pastkey)
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: {{$status3value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status3value}}</font></div>
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: {{$status3value / $pastvalue * 100}}%"><font color="black" font size="3">{{round($status3value / $pastvalue * 100)}}%</font></div>
                     @endif
                 @endforeach
                 </div>
