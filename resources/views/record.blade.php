b37044c4845d2f08ef642c42b7ab62dbd030bd44
tn-luper <tatsuki.nakategawa@gmail.com>
2022-02-01 16:27:30 +0900

    過去の達成率のページを追加

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -1,34 +1,38 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Minimum ToDo</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
     @extends('layouts.app')
 
     @section('content')
         <h1>Minimum ToDo</h1>
-        <h2>タスクの達成率</h2>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link" href="/active">今日のタスク</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/past">過去のタスク</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/record"><b>過去の達成率</b></a>
+            </li>
+        </ul>
             @foreach ($pastcounts as $pastkey => $pastvalue)
-                <p>{{$pastkey}}</p>
-                <p>{{$pastvalue}}</p>
+                <p></p>
+                <h3>{{date('Y年m月',  strtotime($pastkey))}}</h3>
+                <h5>総タスク数：{{$pastvalue}}</h5>
+                <div class="progress w-75" style="height: 30px;">
                 @foreach ($status1counts as $status1key => $status1value)
                     @if($status1key === $pastkey)
-                    <p>{{$status1value/$pastvalue}}</p>
+                        <div class="progress-bar bg-success" role="progressbar" style="width: {{$status1value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status1value}}</font></div>
                     @endif
                 @endforeach
                 @foreach ($status2counts as $status2key => $status2value)
                     @if($status2key === $pastkey)
-                    <p>{{$status2value}}</p>
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: {{$status2value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status2value}}</font></div>
                     @endif
                 @endforeach
                 @foreach ($status3counts as $status3key => $status3value)
                     @if($status3key === $pastkey)
-                    <p>{{$status3value}}</p>
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: {{$status3value / $pastvalue * 100}}%"><font color="black" font size="3">{{$status3value}}</font></div>
                     @endif
                 @endforeach
+                </div>
             @endforeach
     @endsection
-</html>
