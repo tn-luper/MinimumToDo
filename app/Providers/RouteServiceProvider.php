b4553f90b30db6e6ef9e08708e1aa932a50ce91d
tn-luper <tatsuki.nakategawa@gmail.com>
2022-02-12 18:05:57 +0900

    登録後にホームページに移動しないエラーを修正

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/active'; //homeから編集
+    public const HOME = '/'; //homeから編集
 
     /**
      * Define your route model bindings, pattern filters, etc.
